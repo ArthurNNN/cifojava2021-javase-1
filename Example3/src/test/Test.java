fcf498f8a44d9c99b7711c7480fd56f73537864c
Arthur <nikitinarthur@gmail.com>
2021-04-22 09:14:39 +0200

    add reading user data

--- Example3/src/test/Test.java
+++ Example3/src/test/Test.java
@@ -1,10 +1,29 @@
 package test;
 
+import java.util.Scanner;
+
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Tell me your name");
+		String userName = reader.nextLine();
+		
+		System.out.println("Tell me your type of user");
+		String userType = reader.nextLine();
 
+		System.out.println("Tell me amount of your buy");
+		String amountBuy = reader.nextLine();
+		
+		System.out.println("User: /n");
+		System.out.println("------------------------------------");
+		System.out.println("User Name: " + userName);
+		System.out.println("User Type: " + userType);
+		System.out.println("Amount buy: " + amountBuy);
+		
+		
 	}
 
 }
