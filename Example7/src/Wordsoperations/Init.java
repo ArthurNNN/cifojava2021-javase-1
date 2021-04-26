f7f5c0a9c0728e82e6229c6487c49b5c03a8f328
Arthur <nikitinarthur@gmail.com>
2021-04-26 09:16:09 +0200

    first commit

---
+++ Example7/src/Wordsoperations/Init.java
@@ -0,0 +1,21 @@
+package Wordsoperations;
+
+import java.util.Scanner;
+
+public class Init {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Write some string ...");
+		String word = reader.nextLine();
+		
+		Operations.print(word);
+		Operations.lineBreak();
+
+	}
+	
+	
+
+}
---
+++ Example7/src/Wordsoperations/Operations.java
@@ -0,0 +1,13 @@
+package Wordsoperations;
+
+public class Operations {
+	public static void print (String wordPrint ){
+		System.out.println(wordPrint);
+	}
+	
+	public static void lineBreak ( ){
+		System.out.println("\n");
+	}
+	
+
+}
