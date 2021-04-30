15d6651676904ab037e58ac4a569964c36e5e9e6
Arthur <nikitinarthur@gmail.com>
2021-04-30 10:19:05 +0200

    light fix

--- AccountsProject/src/accountoperations/AccountCreating.java
+++ AccountsProject/src/accountoperations/AccountCreating.java
@@ -1,17 +1,17 @@
 package accountoperations;
 
-
 public class AccountCreating {
-	
+
 	public static String createUser(String nameAndSurname) {
-		// TODO Auto-generated method stub
+
 		int spacePosition = nameAndSurname.indexOf(' ');
-		String userLogin = (nameAndSurname.substring(0, spacePosition) + nameAndSurname.substring(spacePosition + 1)).toLowerCase();
+		String userLogin = (nameAndSurname.substring(0, spacePosition) + nameAndSurname.substring(spacePosition + 1))
+				.toLowerCase();
 		return userLogin;
 	}
 
 	public static String createEmail(String login) {
-		// TODO Auto-generated method stub
+
 		String email = login + "@java.com";
 		return email;
 	}
@@ -19,21 +19,15 @@ public class AccountCreating {
 	public static void printUserAccount(String user, String email, String password) {
 		// TODO Auto-generated method stub
 
+		System.out.println("Account data:");
+		System.out.println("Login: " + user);
+		System.out.println("Email: " + email);
+		System.out.println("Password: " + password);
 
-		System.out.print("Your user login: ");
-		System.out.println(createUser(user));
-//		Operations.lineBreak();
-
-		System.out.print("Your email: ");
-		System.out.println(createEmail(createUser(email)));
-//		Operations.lineBreak();
-		System.out.print("Your password: ");
-		System.out.println(password);
-//		Operations.lineBreak();
 	}
 
 	public static String createPassword() {
-		// TODO Auto-generated method stub
+
 		String password = "";
 		int count = 0;
 		while (count < 2) {
--- AccountsProject/src/accountoperations/Init.java
+++ AccountsProject/src/accountoperations/Init.java
@@ -6,31 +6,19 @@ import java.util.Scanner;
 public class Init {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 		ArrayList<String> users = new ArrayList<String>();	
 		
 		Scanner reader = new Scanner(System.in);
 		
 
-		System.out.println("Write your name and surname:");
+		System.out.println("Write user (name and surname) to create Acount:");
 		String s = reader.nextLine();
 
-
-//		Operations.lineBreak();
-
 		AccountlManagment.createAccount(s);
-		
-		
-//		Operations.printStringHorizontal(word);
-//		Operations.lineBreak();
-//		Operations.printStringVertical(word);
-//		Operations.lineBreak();
-//		Operations.printStringDiagonal(word);
-//		Operations.lineBreak();
-
-		
+				
 		System.out.println("\nBuy buy my friends...");
+		reader.close();
 	}
 	
 	
